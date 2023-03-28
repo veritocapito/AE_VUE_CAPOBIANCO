2266e88ab5ca68ec7f8b9bb27acbba46efeb2f82
Veronica Capobianco <veritocapito@gmail.com>
2023-03-28 12:38:51 -0300

    superFilter added

--- index.js
+++ index.js
@@ -14,26 +14,46 @@ const app = createApp({
         }
     },
     created(){
-        this.requestEvents()
+        this.requestAllEvents()
     },
     mounted(){
 
     },
     methods: {
-        // define una función para solicitar los eventos
-        requestEvents() {
-            fetch(this.jsonUrl)
+        // define una función para solicitar todos los eventos
+        requestAllEvents() {
+            try {
+                fetch(this.jsonUrl)
                 .then(response => response.json())
                 .then(data => {
                     this.events = data.events
                     this.backupEvents = this.events
+                    this.categoryFilter(data.events)
                 })
-                .catch((error) => console.log(error));
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        //define una propiedad para filtrar por categorias
+        categoryFilter(array){
+            array.forEach(item => {
+                if(!this.categories.includes(item.category)){
+                    this.categories.push(item.category)
+                }
+            });
         }
     },
     computed:{
-        // define una propiedad computada para filtrar
-
+        superFilter(){
+            // define una propiedad computada para filtrar por texto
+            let firstFilter = this.events.filter( item => item.name.toLowerCase().includes(this.text.toLowerCase()))
+            // define una propiedad computada para filtrar por checkbox
+            if (!this.categoriesSelected.length) {
+                this.backupEvents = firstFilter
+            } else {
+                this.backupEvents = firstFilter.filter( item => this.categoriesSelected.includes(item.category))
+            }
+        },
     }
     //inicializa la instancia de Vue
 }).mount('#app')
